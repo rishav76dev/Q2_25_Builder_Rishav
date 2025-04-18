0e1ed8c1c3b4693763e32fad6ac0aead0694cf36
rishav76dev <rjrishav037@gmail.com>
2025-04-18 20:04:38 +0530

    fixed bugs

--- escrow/programs/escrow/src/instructions/refund.rs
+++ escrow/programs/escrow/src/instructions/refund.rs
@@ -1,82 +1,80 @@
-#![allow(unexpected_cfgs)]
-
-use anchor_lang::prelude::CpiContext;
-use anchor_spl::token::TransferChecked;
+use anchor_lang::prelude::*;
+use anchor_spl::{associated_token::AssociatedToken, token_interface::{close_account, transfer_checked, CloseAccount, Mint, TokenAccount, TokenInterface, TransferChecked}};
 
 
+use crate::state::Escrow;
 
 #[derive(Accounts)]
+
 pub struct Refund<'info> {
     #[account(mut)]
     pub maker: Signer<'info>,
-    #[account(
-        mint::token_program = token_program
-    )]
-
     pub mint_a: InterfaceAccount<'info, Mint>,
-
+    pub mint_b: InterfaceAccount<'info, Mint>,
     #[account(
         mut,
         associated_token::mint = mint_a,
+        associated_token::authority = maker,
     )]
-
-    pub maker_ata_a: InterfaceAccount<'info, TokenAccount>,
+    pub maker_mint_a_ata: InterfaceAccount<'info, TokenAccount>,
     #[account(
         mut,
-        close = maker,
-        has_one = mint_a,
-        has_one = maker,
-        seeds = [b"escrow", maker.key().as_ref(), &escrow.seed.to_le_bytes().as_ref()],
+        seeds = [b"escrow", maker.key().as_ref(), escrow.seed.to_le_bytes().as_ref()],
         bump = escrow.bump,
+        close = maker,
     )]
     pub escrow: Account<'info, Escrow>,
-
-    pub associated_token_program: Program<'info, AssociatedToken>,
-    pub token_program: Program<'info, Token>,
+    #[account(
+        mut,
+        associated_token::mint = mint_a,
+        associated_token::authority = escrow,
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
     pub system_program: Program<'info, System>,
-
+   pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Interface<'info, TokenInterface>,
 }
-    
-impl <'info> Refund<'info> {
-    pub fn refund_and_close_vault(
-        &mut self
-    ) ->Result<()> {
-        let signer_seeds: [&[&[u8]]] = &[&[
-            b"escrow",
-            self.maker.to_account_info().key.as_ref(),
-            &self.escrow.seed.to_le_bytes()[..],
-            &[self.escrow.bump],
-        ]];
-        let transfer_accounts = TransferChecked {
-            from: self.maker_ata_a.to_account_info(),
-            mint: self.mint_a.to_account_info(),
-            to: self.vault.to_account_info(),
-            authority: self.maker.to_account_info(),
-           
-        };
 
-        let cpi_ctx = CpiContext::new(cpi_program, transfer_accounts);
-      
-        transfer_checked(
-            cpi_ctx,
-            self.vault.amount,
-            self.mint_a.decimals
-        )?;
+impl<'info> Refund<'info> {
+    pub fn refund(&mut self,) -> Result<()> {
+       let cpi_program = self.token_program.to_account_info();
+       let cpi_accounts = TransferChecked {
+           from: self.vault.to_account_info(),
+           to: self.maker_mint_a_ata.to_account_info(),
+           authority: self.escrow.to_account_info(),
+           mint: self.mint_a.to_account_info(),    
+    };
 
-        let close_accounts = CloseAccount {
-            account: self.vault.to_account_info(),
-            destination: self.maker.to_account_info(),
-            authority: self.maker.to_account_info(),
-        };
-
-        let close_cpi_ctx = CpiContext::new_with_signer(self.token_program.to_account_info,
-            close_accounts,
-             &signer_seeds,
-        );
-        close_account(
-            close_cpi_ctx
-        );
+    let seeds: &[&[&[u8]]] = &[
+            &[
+                b"escrow",
+                self.maker.key.as_ref(),
+                &self.escrow.seed.to_le_bytes(),
+                &[self.escrow.bump],
+            ],
+        ];
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, seeds);
+        transfer_checked(cpi_ctx, self.escrow.receive, self.mint_a.decimals)?;
         Ok(())
     }
-    
+
+    pub fn close(&mut self) -> Result<()> {
+        let cpi_program = self.token_program.to_account_info();
+        let cpi_accounts = CloseAccount {
+            account: self.vault.to_account_info(),
+            destination: self.maker_mint_a_ata.to_account_info(),
+            authority: self.escrow.to_account_info(),
+        };
+        let seeds: &[&[&[u8]]] = &[
+            &[
+                b"escrow",
+                self.maker.key.as_ref(),
+                &self.escrow.seed.to_le_bytes(),
+                &[self.escrow.bump],
+            ],
+        ];
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, seeds);
+        close_account(cpi_ctx)?;
+        Ok(())
+    }
 }
--- escrow/programs/escrow/src/lib.rs
+++ escrow/programs/escrow/src/lib.rs
@@ -1,24 +1,32 @@
-#![allow(unexpected_cfgs)]
-
 use anchor_lang::prelude::*;
-
 
 pub mod instructions;
 pub mod state;
 
-pub use instructions::*;
+use crate::instructions::*;
+
 
 declare_id!("GGkCBhDqweeCQbJRddw5dogQYCZVDiJbjkg9KgNu631k");
 
-#[program]
 pub mod escrow {
     use super::*;
 
-    pub fn make(ctx: Context<Make>, seed: u64, deposit: u64) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn make(ctx: Context<Make>, seed: u64, receive_amount: u64, deposit_amount: u64,) -> Result<()> {
+       ctx.accounts.init_escrow(seed, receive_amount, &ctx.bumps)?;
+       ctx.accounts.deposit(deposit_amount)?;
+        Ok(())
+    }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.transfer_to_maker()?;
+        ctx.accounts.transfer_to_taker()?;
+        ctx.accounts.close_vault()?;
+        Ok(())
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund()?;
+        ctx.accounts.close()?;
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
