1fd51cd5d5863f31058424e7c02ffcd6c7a2f992
rishav76dev <rjrishav037@gmail.com>
2025-08-15 19:18:47 +0000

    refund formatted

--- escrow/programs/escrow/src/instructions/refund.rs
+++ escrow/programs/escrow/src/instructions/refund.rs
@@ -1,7 +1,6 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{associated_token::AssociatedToken, token_interface::{close_account, transfer_checked, CloseAccount, Mint, TokenAccount, TokenInterface, TransferChecked}};
 
-
 use crate::state::Escrow;
 
 #[derive(Accounts)]
@@ -9,29 +8,42 @@ use crate::state::Escrow;
 pub struct Refund<'info> {
     #[account(mut)]
     pub maker: Signer<'info>,
+
+    #[account(
+        mint::token_program = token_program
+    )]
     pub mint_a: InterfaceAccount<'info, Mint>,
     pub mint_b: InterfaceAccount<'info, Mint>,
+
     #[account(
         mut,
         associated_token::mint = mint_a,
         associated_token::authority = maker,
+        associated_token::token_program = token_program
     )]
     pub maker_mint_a_ata: InterfaceAccount<'info, TokenAccount>,
+
     #[account(
-        mut,
-        seeds = [b"escrow", maker.key().as_ref(), escrow.seed.to_le_bytes().as_ref()],
-        bump = escrow.bump,
-        close = maker,
+    mut,
+    close = maker,
+    has_one = mint_a,
+    has_one = maker,
+    seeds = [b"escrow", maker.key().as_ref(), escrow.seed.to_le_bytes().as_ref()],
+    bump = escrow.bump
     )]
     pub escrow: Account<'info, Escrow>,
+
+
     #[account(
         mut,
         associated_token::mint = mint_a,
         associated_token::authority = escrow,
+        associated_token::token_program = token_program
     )]
     pub vault: InterfaceAccount<'info, TokenAccount>,
+
     pub system_program: Program<'info, System>,
-   pub associated_token_program: Program<'info, AssociatedToken>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
     pub token_program: Interface<'info, TokenInterface>,
 }
 
@@ -42,7 +54,7 @@ impl<'info> Refund<'info> {
            from: self.vault.to_account_info(),
            to: self.maker_mint_a_ata.to_account_info(),
            authority: self.escrow.to_account_info(),
-           mint: self.mint_a.to_account_info(),    
+           mint: self.mint_a.to_account_info(),
     };
 
     let seeds: &[&[&[u8]]] = &[
@@ -62,7 +74,7 @@ impl<'info> Refund<'info> {
         let cpi_program = self.token_program.to_account_info();
         let cpi_accounts = CloseAccount {
             account: self.vault.to_account_info(),
-            destination: self.maker_mint_a_ata.to_account_info(),
+            destination: self.maker.to_account_info(),
             authority: self.escrow.to_account_info(),
         };
         let seeds: &[&[&[u8]]] = &[
