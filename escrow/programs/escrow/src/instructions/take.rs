3377d4e2b48f0fcd04600b2fea027279f449add9
rishav76dev <rjrishav037@gmail.com>
2025-08-16 22:42:29 +0000

    Restore modules after failed submodule split

---
+++ escrow/programs/escrow/src/instructions/take.rs
@@ -0,0 +1,138 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{
+        close_account, transfer_checked, CloseAccount, Mint, TokenAccount, TokenInterface,
+        TransferChecked,
+    },
+};
+use crate::state::Escrow;
+
+#[derive(Accounts)]
+#[instruction(seed:u64)]
+pub struct Take<'info> {
+    #[account(mut)]
+    pub taker: Signer<'info>,
+
+    #[account(mut)]
+    pub maker: SystemAccount<'info>,
+
+    #[account(mint::token_program = token_program)]
+    pub mint_a: InterfaceAccount<'info, Mint>,
+
+    #[account(mint::token_program = token_program)]
+    pub mint_b: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        init_if_needed,
+        payer = taker,
+        associated_token::mint = mint_a,
+        associated_token::authority = taker,
+        associated_token::token_program = token_program
+    )]
+    pub taker_mint_a_ata: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_b,
+        associated_token::authority = taker,
+        associated_token::token_program = token_program
+    )]
+    pub taker_mint_b_ata: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        init_if_needed,
+        payer = taker,
+        associated_token::mint = mint_b,
+        associated_token::authority = maker,
+        associated_token::token_program = token_program
+    )]
+    pub maker_mint_b_ata: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        has_one = maker,
+        has_one = mint_a,
+        has_one = mint_b,
+        close = maker,
+        seeds = [b"escrow", maker.key().as_ref(), escrow.seed.to_le_bytes().as_ref()],
+        bump = escrow.bump,
+    )]
+    pub escrow: Account<'info, Escrow>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_a,
+        associated_token::authority = escrow,
+        associated_token::token_program = token_program
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+
+    pub system_program: Program<'info, System>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Interface<'info, TokenInterface>,
+}
+
+
+impl<'info> Take<'info> {
+    pub fn transfer_to_maker(&mut self,) -> Result<()> {
+        let cpi_program = self.token_program.to_account_info();
+        let receive_amount = self.escrow.receive;
+        let cpi_accounts = TransferChecked {
+            from: self.taker_mint_b_ata.to_account_info(),
+            to: self.maker_mint_b_ata.to_account_info(),
+            authority: self.taker.to_account_info(),
+            mint: self.mint_b.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        transfer_checked(cpi_ctx, receive_amount, self.mint_b.decimals)?;
+        Ok(())
+    }
+
+    pub fn transfer_to_taker(&mut self) -> Result<()> {
+        let cpi_program = self.token_program.to_account_info();
+        let deposit_amount = self.vault.amount;
+        let cpi_accounts = TransferChecked {
+            from: self.vault.to_account_info(),
+            to: self.taker_mint_a_ata.to_account_info(),
+            mint: self.mint_a.to_account_info(),
+            authority: self.escrow.to_account_info(),
+        };
+
+        let binding = self.maker.key();
+        let seeds: &[&[&[u8]]] = &[
+            &[
+                b"escrow",
+                binding.as_ref(),
+                &self.escrow.seed.to_le_bytes(),
+                &[self.escrow.bump],
+            ],
+        ];
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, seeds);
+        transfer_checked(cpi_ctx, deposit_amount, self.mint_a.decimals)?;
+        Ok(())
+    }
+
+    pub fn close_vault(&mut self) -> Result<()> {
+        let cpi_program = self.token_program.to_account_info();
+        let cpi_accounts = CloseAccount {
+            account: self.vault.to_account_info(),
+            destination: self.maker.to_account_info(),
+            authority: self.escrow.to_account_info(),
+        };
+
+        let binding = self.maker.key();
+        let seeds: &[&[&[u8]]] = &[
+            &[
+                b"escrow",
+                binding.as_ref(),
+                &self.escrow.seed.to_le_bytes(),
+                &[self.escrow.bump],
+            ],
+        ];
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, seeds);
+        close_account(cpi_ctx)?;
+        Ok(())
+    }
+}
---
+++ escrow/tests/escrow.ts
@@ -0,0 +1,155 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Escrow } from "../target/types/escrow";
+import { createAssociatedTokenAccountIdempotentInstruction, createInitializeAccount2Instruction, createInitializeMint2Instruction, createMintToInstruction, getAssociatedTokenAddressSync, getMinimumBalanceForRentExemptMint, MINT_SIZE, TOKEN_2022_PROGRAM_ID } from "@solana/spl-token";
+import { Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
+import { BN } from "bn.js";
+import * as fs from "fs";
+import { randomBytes } from "crypto";
+describe("escrow", () => {
+  // Configure the client to use the local cluster.
+
+  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.getProvider();
+  const program = anchor.workspace.escrow as Program<Escrow>;
+
+  const tokenProgram = TOKEN_2022_PROGRAM_ID;
+
+  const seed = new BN(randomBytes(8));
+
+   const log = async (signature: string): Promise<string> => {
+    console.log(
+      `Your transaction signature: https://explorer.solana.com/transaction/${signature}?cluster=custom&customUrl=${provider.connection.rpcEndpoint}`
+    );
+    return signature;
+  };
+
+  const confirm = async (signature: string): Promise<string> => {
+    const block = await provider.connection.getLatestBlockhash();
+    await provider.connection.confirmTransaction({
+      signature,
+      ...block,
+    });
+    return signature;
+  };
+
+  const [maker, taker, mintA, mintB] = Array.from({ length: 4 }, () =>
+    Keypair.generate()
+  );
+
+  const [maker_mint_a_ata, maker_mint_b_ata, taker_mint_a_ata, taker_mint_b_ata] = [maker, taker]
+      .map((a) =>
+        [mintA, mintB].map((m) =>
+          getAssociatedTokenAddressSync(m.publicKey, a.publicKey, false, TOKEN_2022_PROGRAM_ID)
+    )
+    )
+    .flat();
+
+     const escrow = PublicKey.findProgramAddressSync(
+      [Buffer.from("escrow"), maker.publicKey.toBuffer(), seed.toArrayLike(Buffer, "le", 8)],
+      program.programId
+    )[0];
+
+     const vault = getAssociatedTokenAddressSync(mintA.publicKey, escrow, true, TOKEN_2022_PROGRAM_ID);
+
+
+    const accounts = {
+      maker: maker.publicKey,
+      taker: taker.publicKey,
+      mintA: mintA.publicKey,
+      mintB: mintB.publicKey,
+      maker_mint_a_ata,
+      maker_mint_b_ata,
+      taker_mint_a_ata,
+      taker_mint_b_ata,
+      escrow,
+      vault,
+      tokenProgram,
+    }
+
+ it("Airdrop and create mints", async () => {
+  const lamports = await getMinimumBalanceForRentExemptMint(provider.connection);
+  const tx = new Transaction();
+
+  // Fund maker and taker
+  tx.add(
+    ...[maker, taker].map((account) =>
+      SystemProgram.transfer({
+        fromPubkey: provider.wallet.publicKey,
+        toPubkey: account.publicKey,
+        lamports: 10 * LAMPORTS_PER_SOL,
+      })
+    )
+  );
+
+  // Create mint accounts
+  tx.add(
+    ...[mintA, mintB].map((mint) =>
+      SystemProgram.createAccount({
+        fromPubkey: provider.wallet.publicKey,
+        newAccountPubkey: mint.publicKey,
+        lamports,
+        space: MINT_SIZE,
+        programId: TOKEN_2022_PROGRAM_ID,
+      })
+    )
+  );
+  tx.add(
+    ...[
+      { mint: mintA, authority: maker, ata: maker_mint_a_ata },
+      { mint: mintB, authority: taker, ata: taker_mint_b_ata },
+    ].flatMap((x) => [
+      createInitializeMint2Instruction(x.mint.publicKey, 6, x.authority.publicKey, null, TOKEN_2022_PROGRAM_ID),
+      createAssociatedTokenAccountIdempotentInstruction(provider.wallet.publicKey, x.ata, x.authority.publicKey, x.mint.publicKey, TOKEN_2022_PROGRAM_ID),
+      createMintToInstruction(x.mint.publicKey, x.ata, x.authority.publicKey, 1e9, undefined, TOKEN_2022_PROGRAM_ID),
+    ])
+  );
+
+  // Send the transaction
+  await provider.sendAndConfirm(tx, [maker, taker, mintA, mintB]).then(log);
+});
+
+ it("Make", async () => {
+    // Add your test here.
+    const tx = await program.methods
+    .make(seed, new BN(1e6), new BN(1e6))
+    .accounts({ ...accounts })
+    .signers([maker])
+    .rpc()
+    .then(confirm)
+    .then(log);
+
+    console.log("Makeing of Escrow completed", tx);
+
+  });
+
+  xit("Refund", async () => {
+    const tx = await program.methods
+    .refund()
+    .accounts({ ...accounts })
+    .signers([maker])
+    .rpc()
+    .then(confirm)
+    .then(log);
+
+    console.log("Refund Completed", tx);
+  });
+
+   it("Take", async () => {
+    try{
+      const tx = await program.methods
+        .take(seed)
+        .accounts({ ...accounts })
+        .signers([taker])
+        .rpc()
+        .then(confirm)
+        .then(log);
+
+    console.log("take confirmed", tx);
+    }catch(e){
+      console.log(e);
+      throw(e);
+    };
+
+  });
+});
